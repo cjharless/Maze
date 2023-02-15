3f4aa12175a19b8de3303953fb89de03edce0dbd
cjharless <37884689+cjharless@users.noreply.github.com>
2023-02-14 22:12:26 -0700

    add Maze unity files

---
+++ Assets/Scripts/CheckWin.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckWin : MonoBehaviour
+{
+    public GameObject winMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winMessage.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        //we won
+        if (!winMessage.activeInHierarchy)
+            winMessage.SetActive(true);
+    }
+}
